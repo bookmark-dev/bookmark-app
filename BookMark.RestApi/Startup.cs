ba8202e23555541b52ef5353aadb7e829dd1028c
April13 <aprilnguyen13@gmail.com>
2020-04-06 02:18:33 -0500

    add usecors in configureservices

--- BookMark.RestApi/Startup.cs
+++ BookMark.RestApi/Startup.cs
@@ -23,6 +23,7 @@ namespace BookMark.RestApi {
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services) {
             string base_url = Environment.GetEnvironmentVariable("RestApiUrl");
+            services.AddCors();
             services.AddControllers();
             services.AddDbContext<BookMarkDbContext>(options => {
                 options.UseSqlServer(Configuration.GetConnectionString(base_url == null ? "local" : "docker"));

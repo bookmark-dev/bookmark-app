5186cad85dd04fa3896fefeb5b9982bf7b03d422
Tyler Cadena <tylercadena@alum.calarts.edu>
2020-04-06 22:11:39 -0500

    progress on angular

--- BookMark.Angular/public/src/app/http.service.ts
+++ BookMark.Angular/public/src/app/http.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { env } from 'process';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
-  constructor() { }
+  constructor(private _http: HttpClient, private _url: string) {
+    this._url = env['RestApiUrl'];
+    if (this._url.length == 0) {
+      this._url = 'https://localhost:5001';
+    }
+  }
+  getOrganization(id: string) {
+    return this._http.get(`${this._url}/api/org/${id}`);
+  }
+  getUser(id: string) {
+    return this._http.get(`${this._url}/api/user/${id}`);
+  }
 }
